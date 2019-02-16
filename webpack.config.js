141ff411bed77080da14cc3b5e1f134b60d0ad0d
yogeshvar <yogeshvar@icloud.com>
2019-02-16 09:52:17 +0530

    update entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, "/src/index.html"),
   filename: "./index.html"
 });
 module.exports = {
-  entry: path.join(__dirname, "./src/index.js"),
+  entry: path.join(__dirname, "./src/index.jsx"),
   output: {
     path: path.join(__dirname, "./dist/index.js"),
     filename: "bundle.js"
